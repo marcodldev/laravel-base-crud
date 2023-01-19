f59d68753ce18abf319c4628429ad951e419b725
marcodldev <marcodelisi2@gmail.com>
2023-01-19 17:28:37 +0100

    +

--- database/migrations/2023_01_19_154705_create_comics_table.php
+++ database/migrations/2023_01_19_154705_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series', 100);
+            $table->string('sale_date', 100);
+            $table->string('type', 100);
             $table->timestamps();
         });
     }
--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -11,6 +11,6 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // $array_fumetti = config('comics')
     }
 }
